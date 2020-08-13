fdcb5eb43ea30b439b9ffdd6f09859b3424e78e3
Jose V <josepablov@gmail.com>
2020-08-13 00:46:13 -0600

    netlify wip

--- src/functions/fetchmovielist.js
+++ src/functions/fetchmovielist.js
@@ -1,4 +1,12 @@
 exports.handler = function (event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: 'Hello, World'
+  })
+};
+
+
+exports.handler = function (event, context, callback) {
   const fetchMovies = async () => {
     const result =
       (await axios.get) <
